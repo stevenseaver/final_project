a240a1a4af63a6eb08ba15bcdcd9cf1c63cf9b75
stevenseaver <56994967+stevenseaver@users.noreply.github.com>
2020-04-23 15:31:24 +0700

    Add files via upload
    This is the script used in my UNITY gaming development for Post-Stroke Rehab. System

---
+++ PauseMenu.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO.Ports;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // serial PORT, when loading to main screen, pause serial comm
+    SerialPort sp = new SerialPort("/dev/cu.usbserial", 115200);
+
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 1; //timeout to stabilize serial readout
+    }
+
+    // Update is called once per frame
+    // gameIsOver checks if game state is over, if true, then we shouldn't load
+    // PauseMenu. Hence, return null
+    void Update()
+    {
+        if (GameOverMenu.gameIsOver == true)
+            return;
+        else
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }   
+    }
+
+    // Resume Gaming Runtime, setting pauseMenuUI disabled and gameIsPausde false,
+    // then opens up the serial line with '1'
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+
+        gameIsPaused = false;
+        Debug.Log("Resuming ...  ");
+        sp.WriteLine("1"); //resume comm
+    }
+    //Pause Gaming Runtime
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        
+        gameIsPaused = true;
+        sp.WriteLine("2"); //turn off comm while paused
+    }
+    //load main menu when button menu pressed
+    public void loadMenu()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Load menu ...  ");
+        SceneManager.LoadScene("Main Menu");
+
+        gameIsPaused = true;
+        sp.WriteLine("2");
+    }
+    //restart game
+    public void Restart()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        //sp.WriteLine("1"); //resume comm
+
+        gameIsPaused = false;
+        Debug.Log("Restarting ...  ");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    //quit game
+    public void quitGame()
+    {
+        Application.Quit(1);
+    }
+}
---
+++ Score.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public Text scoreText;
+
+    public static string score = "0";
+    public float timeScore, timeReduction;
+
+    private void Start()
+    {
+        timeScore = 0f;
+        timeReduction -= 1f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        score = timeScore.ToString("0");
+        scoreText.text = score;  //get value from time AT this exact moment
+        timeScore += -timeReduction * Time.deltaTime;
+        // increase the timeScore with +(-timereduction)
+        // which equals to timeScore = timeScore + -(-1) hence timeScore = timeScore + 1;
+    }
+}
